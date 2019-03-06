efbfdcef2d94a4229a27801d7d9b735db0af2e88
rapahaeru <rapahaeru@gmail.com>
2019-03-06 10:35:52 -0300

    Create a Header component to simulate structure

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Styled from "styled-components";
+import Grid from "@material-ui/core/Grid";
+
+const HeaderStyled = Styled.header`
+  background-color: tomato;
+  height: 60px;
+  padding: 10px 0;
+`;
+
+const Header = () => {
+  return (
+    <HeaderStyled>
+      <Grid container>
+        <Grid item xs={12}>
+          Header inicial aqui
+        </Grid>
+      </Grid>
+    </HeaderStyled>
+  );
+};
+
+export default Header;
