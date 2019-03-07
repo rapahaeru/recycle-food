ccc7e076ab15134b53c68c23fca52e41c4dacbc3
Raphael Oliveira <rapahaeru@gmail.com>
2019-03-07 09:51:11 -0300

    implements eslint patterns

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,3 +1,6 @@
+/* eslint no-use-before-define: ["error", { "functions": false }] */
+/* eslint no-param-reassign: ["error", { "props": false }] */
+
 // This optional code is used to register a service worker.
 // register() is not called by default.
 
