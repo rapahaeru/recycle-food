6be24b70346592af637f67825859a7144e4612cf
Raphael Oliveira <rapahaeru@gmail.com>
2019-03-10 12:44:25 -0300

    update layout status and create Ingredients coponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,102 @@
 import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import Header from './components/Header';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import AppBar from '@material-ui/core/AppBar';
+import Icon from '@material-ui/core/Icon';
+import Ingredients from './components/Ingredients';
 
-const App = () => (
-  <Grid container>
-    <Grid item xs={12}>
-      <Header />
-    </Grid>
-    <Grid
-      container
-      direction="column"
-      justify="center"
-      alignItems="center"
-    >
-      <Grid item xs={12}>
-        <h2>O que posso possuo.</h2>
-      </Grid>
-    </Grid>
+
+const styles = theme => ({
+  root: {
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2,
+  },
+  container: {
+    marginTop: theme.spacing.unit * 2,
+    marginBottom: theme.spacing.unit * 2,
+  },
+  wrapper: {
+    paddingLeft: theme.spacing.unit * 2,
+    paddingRight: theme.spacing.unit * 2,
+  },
+  appBar: {
+    padding: theme.spacing.unit * 2,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  title: {
+    color: 'white',
+    paddingLeft: theme.spacing.unit * 2,
+  },
+  subtitle: {
+    marginTop: theme.spacing.unit * 2,
+    marginBottom: theme.spacing.unit * 2,
+  },
+  paperWrapper: {
+    padding: theme.spacing.unit * 2,
+  },
+});
+
+const App = (props) => {
+  const { classes } = props;
+
+  return (
     <Grid container>
-      <Grid item xs={12} sm={6}>
-        ITEM DE LISTAGEM
+      <Grid item xs={12}>
+        <AppBar position="static" className={classes.appBar}>
+          <Icon>remove_shopping_cart</Icon>
+          <Typography variant="h6" component="h1" inline className={classes.title}>Recycle food</Typography>
+        </AppBar>
       </Grid>
-      <Grid item xs={12} sm={6}>
-        ITEM DE LISTAGEM
+      <Grid container className={classes.wrapper}>
+        <Grid
+          container
+          direction="column"
+          justify="center"
+          alignItems="center"
+        >
+          <Grid className={classes.container} item xs={12}>
+            <Typography variant="h4">Lorem ipsum</Typography>
+            <Typography variant="p">
+              Lorem ipsum dolor avec Lorem ipsum dolor
+              avec Lorem ipsum dolor avec Lorem ipsum dolor avec Lorem ipsum dolor
+              avec Lorem ipsum dolor avec.
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid container spacing={24}>
+          <Grid item xs={12} sm={6}>
+            <section>
+              <Typography variant="h5" className={classes.subtitle}>
+                O que tenho:
+              </Typography>
+              <Paper className={classes.paperWrapper}>
+                <h3>Testando paper</h3>
+                <Ingredients />
+              </Paper>
+            </section>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <section>
+              <Typography variant="h5" className={classes.subtitle}>
+                O que posso fazer:
+              </Typography>
+              <Paper className={classes.paperWrapper}>
+                <h3>Testando paper</h3>
+                <ul>
+                  <li>testando</li>
+                  <li>testando</li>
+                  <li>testando</li>
+                </ul>
+              </Paper>
+            </section>
+          </Grid>
+        </Grid>
       </Grid>
     </Grid>
+  );
+};
 
-
-  </Grid>
-);
-
-export default App;
+export default withStyles(styles)(App);
---
+++ src/components/Ingredients/Ingredients.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Checkbox from '@material-ui/core/Checkbox';
+import { withStyles } from '@material-ui/core';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+});
+
+class Ingredients extends React.Component {
+  state = {
+    checked: [0],
+  };
+
+  handleToggle = value => () => {
+    const { checked } = this.state;
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    this.setState({
+      checked: newChecked,
+    });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { checked } = this.state;
+
+    return (
+      <List className={classes.root}>
+        {[0, 1, 2, 3].map(value => (
+          <ListItem key={value} role={undefined} dense button onClick={this.handleToggle(value)}>
+            <Checkbox
+              checked={checked.indexOf(value) !== -1}
+              tabIndex={-1}
+              disableRipple
+            />
+            <ListItemText primary={`Line item ${value + 1}`} />
+          </ListItem>
+        ))}
+      </List>
+    );
+  }
+}
+
+Ingredients.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Ingredients);
